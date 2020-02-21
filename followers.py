e2215c90cdb7ebe22628700104e3db7b94e091bb
Billy Okeyo <34124094+cartel360@users.noreply.github.com>
2020-02-21 04:02:43 +0300

    Update followers.py

--- followers.py
+++ followers.py
@@ -1,11 +1,18 @@
 from bs4 import BeautifulSoup
 import requests
+
 handle = input('Input your account name on Twitter: ') 
 temp = requests.get('https://twitter.com/'+handle)
 bs = BeautifulSoup(temp.text,'lxml')
+
 try:
     follow_box = bs.find('li',{'class':'ProfileNav-item ProfileNav-item--followers'})
     followers = follow_box.find('a').find('span',{'class':'ProfileNav-value'})
     print("Number of followers: {} ".format(followers.get('data-count')))
+    
+    following_box = bs.find('li',{'class':'ProfileNav-item ProfileNav-item--following'})
+    following = following_box.find('a').find('span',{'class':'ProfileNav-value'})
+    print("{} is following {} people.".format(handle,following.get('data-count')))
+
 except:
     print('Account name not found...')
